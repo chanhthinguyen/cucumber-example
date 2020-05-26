09bb1909d6688625d96d1df12b7a4cd6a700a5e9
thinguyenwizeline <thi.nguyen@wizeline.com>
2020-05-26 14:11:13 +0700

    Add first commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features/api"},
                  glue = {"steps.api"},
-                 plugin = {"pretty","html:target/cucumber-html-report","json:target/cucumber-reports/Cucumber.json"},
+                 plugin = {"pretty","html:target/cucumber-html-report","json:reports/cucumber-reports/Cucumber.json"},
                  strict = false)
 public class TestRunner  {
 }
